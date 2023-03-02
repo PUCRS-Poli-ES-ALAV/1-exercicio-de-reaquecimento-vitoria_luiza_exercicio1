38e0cd429368fcf00b2150b0e5bfe960687ccd18
Vitoria Ebeling Hahn <vitoria.hahn@edu.pucrs.br>
2023-03-02 18:13:40 -0300

    erro quando fui fazer o commit hehe

--- Exercicio3.js
+++ Exercicio3.js
@@ -1,10 +1,9 @@
-function incrementosSucessivos(n) {
-    if(n === 1) {
-      return 1;
-    } else {
-      return 1/n + incrementosSucessivos(n-1);
-    }
+function calculaFracao(n) {
+  if(n === 1) {
+    return 1;
+  } else {
+    return 1/n + incrementosSucessivos(n-1);
   }
+}
 
 console.log(incrementosSucessivos(2));
-  
